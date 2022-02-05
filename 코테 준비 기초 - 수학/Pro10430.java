8381376deeae139abb1e819fa878a5fca614fac8
Jisoo Hwang <77599304+plum-king@users.noreply.github.com>
2022-02-05 17:49:17 +0900

    upload #10430. #17427
    코테 준비 기초 - 수학

---
+++ 코테 준비 기초 - 수학/Pro10430.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Pro10430 {
+    public static void main(String[] args){
+        Scanner scn = new Scanner(System.in);
+        int a = scn.nextInt();
+        int b = scn.nextInt();
+        int c = scn.nextInt();
+        System.out.println((a+b)%c);
+        System.out.println(((a%c)+(b%c))%c);
+        System.out.println((a*b)%c);
+        System.out.println(((a%c)*(b%c))%c);
+    }
+}
