3b7efea1fa1099d2c57dd57ca2414abfa22b6fc1
plum-king <jeesoo0908@gmail.com>
2024-01-15 00:13:51 +0900

    트리_9372, 브루트포스_19532, 구현_18311

---
+++ 구현/18311.py
@@ -0,0 +1,17 @@
+import sys
+input = sys.stdin.readline
+
+n, k = map(int,input().split())
+m = list(map(int,input().split()))
+
+for i in range(n):
+    k -= m[i]
+    if k < 0:
+        print(i+1)
+        break
+else:
+    for i in range(n-1,-1,-1):
+        k -= m[i]
+        if k < 0:
+            print(i+1)
+            break
---
+++ 트리/9372.py
@@ -0,0 +1,14 @@
+import sys
+input = sys.stdin.readline
+
+t = int(input())
+
+for i in range(t):
+    n, m = map(int, input().split())
+    
+    for j in range(m):
+        a, b = map(int, input().split())
+        
+    print(n - 1)
+
+# 나중에 bfs로 다시 풀어보기
